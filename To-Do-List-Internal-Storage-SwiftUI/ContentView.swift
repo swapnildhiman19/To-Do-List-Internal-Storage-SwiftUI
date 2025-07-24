00124399457b09cd06435a63b6d467ac3d702108
s0d0bla <swapnil.dhiman@walmart.com>
2025-07-24 19:24:36 +0530

    Initial Commit

---
+++ To-Do-List-Internal-Storage-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  To-Do-List-Internal-Storage-SwiftUI
+//
+//  Created by Swapnil Dhiman on 24/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ To-Do-List-Internal-Storage-SwiftUI/To_Do_List_Internal_Storage_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_List_Internal_Storage_SwiftUIApp.swift
+//  To-Do-List-Internal-Storage-SwiftUI
+//
+//  Created by Swapnil Dhiman on 24/07/25.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_List_Internal_Storage_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
