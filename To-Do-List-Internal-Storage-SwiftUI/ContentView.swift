f8a5772350fb380b0ec1b8072b5cdc0238fa370a
swapnildhiman19 <swapnildhiman1999@gmail.com>
2025-08-04 19:55:46 +0530

    Implement and Understand UserDefault to persist data locally, along with JSONDecoder and JSONEncoder to convert ItemDetails into Data format to persist

--- To-Do-List-Internal-Storage-SwiftUI/ContentView.swift
+++ To-Do-List-Internal-Storage-SwiftUI/ContentView.swift
@@ -7,21 +7,23 @@
 
 import SwiftUI
 
-struct ToDoList: Identifiable {
+struct ToDoList: Identifiable, Codable {
     let id = UUID()
     var string: String
     var isChecked : Bool = false
 }
 
 struct ContentView: View {
-    @State var pendignItems : [ToDoList] = [
-        ToDoList(string: "Learn AI from scratch"),
-        ToDoList(string: "iOS"),
-        ToDoList(string: "Resume building"),
-        ToDoList(string: "Learn Flutter"),
-        ToDoList(string: "Interview preparation"),
-        ToDoList(string: "DSA")
-    ]
+//    @State var pendignItems : [ToDoList] = [
+//        ToDoList(string: "Learn AI from scratch"),
+//        ToDoList(string: "iOS"),
+//        ToDoList(string: "Resume building"),
+//        ToDoList(string: "Learn Flutter"),
+//        ToDoList(string: "Interview preparation"),
+//        ToDoList(string: "DSA")
+//    ]
+    let defaultsKey = "ToDoList"
+    @State var pendignItems : [ToDoList] = []
 
     @State private var showAddAlert: Bool = false
     @State private var newItemText: String = ""
@@ -41,6 +43,7 @@ struct ContentView: View {
                 .contentShape(Rectangle()) // Make the entire row tappable
                 .onTapGesture {
                     item.isChecked.toggle() // Toggle the isChecked state
+                    saveItems()
                 }
             }
             .navigationTitle("To Do List")
@@ -71,6 +74,7 @@ struct ContentView: View {
                     if !newItemText.isEmpty {
                         let newItem = ToDoList(string: newItemText)
                         pendignItems.append(newItem)
+                        saveItems()
                         newItemText = "" // Reset the text field
                     }
                 }
@@ -83,6 +87,23 @@ struct ContentView: View {
                 Text("Please enter the new item")
             }
         }
+        .onAppear(perform: loadItems)
+    }
+    func saveItems() {
+        //save the items in UserDefaults
+        // Using JSONEncoder to convert the ItemsDetails -> "Data" format
+        UserDefaults.standard.set(try? JSONEncoder().encode(pendignItems), forKey: defaultsKey)
+    }
+    func loadItems() {
+        // need to use JSONDecoder to convert the "Data" from Defaults to struct ToDoList
+        let savedData = UserDefaults.standard.data(forKey: defaultsKey) ?? Data()
+        let decoder = JSONDecoder()
+        let decodedItems: [ToDoList]? = try? decoder.decode([ToDoList].self, from: savedData)
+        if let decodedItems = decodedItems {
+            pendignItems = decodedItems
+        }
+        
+        print(print(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last! as String))
     }
 }
 
